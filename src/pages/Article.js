1b02c8b6a4b02adc13ab99761fb1fe2d67e71b82
Sepid-Mir <sepid.mir@gmail.com>
2023-07-16 19:03:29 -0400

    display content of each article

--- src/pages/Article.js
+++ src/pages/Article.js
@@ -1,4 +1,18 @@
-const Article= () => {
-  return <div> This is my article:</div>
+import articles from './article-content.js';
+import { useParams } from 'react-router-dom';
+const Article = () => {
+  const params = useParams();
+  const articleId = params.id;
+  const article = articles.find(article => article.name === articleId)
+  return (
+    <>
+      <h2> {article.content} </h2>
+      {article.content.map(p => {
+        <p>
+          {p}
+        </p>
+      })}
+
+    </>)
 }
 export default Article;
