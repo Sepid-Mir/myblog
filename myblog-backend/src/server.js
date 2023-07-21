0b6cd68d5bcd11e9a7b14211817b3f401fef0f06
Sepid-Mir <sepid.mir@gmail.com>
2023-07-20 23:48:47 -0400

    test with postman

--- myblog-backend/src/server.js
+++ myblog-backend/src/server.js
@@ -1,6 +1,8 @@
 import express from 'express';
 const app = express();
-app.get('/hello', (req, res) => {
-  res.send('Hello there!');
+app.use(express.json());
+app.post('/hello', (req, res) => {
+  console.log(`Hello ${req.body.name}!`);
+  res.send('hello');
 })
 app.listen(8000, () => console.log('server listening to port 8000'));
