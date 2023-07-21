7793556452dfde2d6784d47eaae707a20208b6fc
Sepid-Mir <sepid.mir@gmail.com>
2023-07-21 13:32:20 -0400

    upvote articles

--- myblog-backend/src/server.js
+++ myblog-backend/src/server.js
@@ -1,11 +1,29 @@
 import express from 'express';
+let articlesInfo = [
+  {
+    name: 'learn-react',
+    upvote: 0
+  },
+  {
+    name: 'learn-node',
+    upvote: 0
+  },
+  {
+    name: 'mongodb',
+    upvote: 0
+  }
+]
 const app = express();
 app.use(express.json());
-app.post('/hello', (req, res) => {
-  res.send(`Hello ${req.body.name}!`);
-})
-app.get('/hello/:name', (req, res) => {
-  const { name } = req.params;
-  res.send(`Hello ${name}!`);
+app.put('/api/articles/:name/upvote', (req, res) => {
+  let { name } = req.params;
+  let article = articlesInfo.find(article => (article.name === name));
+  if (article) {
+    article.upvote += 1;
+    res.send(`The ${name} article now got ${article.upvote} upvotes!`)
+  }
+  else {
+    res.send('Article not found!');
+  }
 })
 app.listen(8000, () => console.log('server listening to port 8000'));
