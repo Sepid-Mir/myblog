0f4e404c5b4c2b9e99117e2224dad12c235f008e
Sepid-Mir <sepid.mir@gmail.com>
2023-07-21 00:17:36 -0400

    test postman with params

--- myblog-backend/src/server.js
+++ myblog-backend/src/server.js
@@ -2,7 +2,10 @@ import express from 'express';
 const app = express();
 app.use(express.json());
 app.post('/hello', (req, res) => {
-  console.log(`Hello ${req.body.name}!`);
-  res.send('hello');
+  res.send(`Hello ${req.body.name}!`);
+})
+app.get('/hello/:name', (req, res) => {
+  const { name } = req.params;
+  res.send(`Hello ${name}!`);
 })
 app.listen(8000, () => console.log('server listening to port 8000'));
