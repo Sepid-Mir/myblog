57aa9d912351c7b53735517bb13fff29b1dcec80
Sepid-Mir <sepid.mir@gmail.com>
2023-07-27 17:49:05 -0400

    rewrite upvote using mongodb

--- myblog-backend/src/server.js
+++ myblog-backend/src/server.js
@@ -35,16 +35,21 @@ app.get('/api/articles/:name', async (req, res) => {
 
 })
 
-app.put('/api/articles/:name/upvote', (req, res) => {
+app.put('/api/articles/:name/upvote', async (req, res) => {
   let { name } = req.params;
-  let article = articlesInfo.find(article => (article.name === name));
-  if (article) {
-    article.upvote += 1;
-    res.send(`The ${name} article now got ${article.upvote} upvotes!!!`)
-  }
-  else {
-    res.send('Article not found!');
-  }
+// let article = articlesInfo.find(article => (article.name === name));
+const client = new MongoClient('mongodb://127.0.0.1');
+await client.connect();
+const db = client.db('react-blog-db');
+await db.collection('articles').updateOne({ name }, { $inc: { upvote: 1 } });
+const article = await db.collection('articles').findOne({ name });
+if (article) {
+  // article.upvote += 1;
+  res.send(`The ${name} article now got ${article.upvote} upvotes!!!`)
+}
+else {
+  res.send('Article not found!');
+}
 })
 app.post('/api/articles/:name/comments', (req, res) => {
 
