a598c8743bfacb5c4206ba9edf44f1b753cc1f19
Sepid-Mir <sepid.mir@gmail.com>
2023-07-28 21:46:59 -0400

    added hooks

--- myblog-frontend/src/pages/Article.js
+++ myblog-frontend/src/pages/Article.js
@@ -1,13 +1,20 @@
 import articles from './article-content.js';
 import { useParams } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 const Article = () => {
+  const [articleInfo, setArticleInfo] = useState({ upvote: 0, comments: [] });
+  useEffect(() => {
+    setArticleInfo({ upvote: 3, comments: [] });
+  })
   const params = useParams();
   const articleId = params.id;
   const article = articles.find(article => article.name === articleId)
   if (!article) return <h1> 404: Page Not Found!</h1>
+  
   return (
     <>
       <h2> {article.title} </h2>
+      <p> The article has {articleInfo.upvote} upvote(s)!</p>
       {article.content.map(paragraph => (
         <p>
           {paragraph}
