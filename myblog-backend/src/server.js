4b900c136aba1cc2dc8005ba7e3bca64d4238b8c
Sepid-Mir <sepid.mir@gmail.com>
2023-07-29 23:22:58 -0400

    added upvote button

--- myblog-backend/src/server.js
+++ myblog-backend/src/server.js
@@ -1,15 +1,11 @@
 import express from 'express';
 import { db, connectToDb } from './db.js';
-import { MongoClient } from 'mongodb';
 
 const app = express();
 app.use(express.json());
 app.get('/api/articles/:name', async (req, res) => {
   const { name } = req.params;
   console.log(name);
-  // const client = new MongoClient('mongodb://127.0.0.1:27017');
-  // await client.connect();
-  // const db = client.db('react-blog-db'); //use react-blog-db
   const article = await db.collection('articles').findOne({ name: name });
   if (article) {
     res.json(article);
@@ -22,15 +18,10 @@ app.get('/api/articles/:name', async (req, res) => {
 
 app.put('/api/articles/:name/upvote', async (req, res) => {
   let { name } = req.params;
-  // let article = articlesInfo.find(article => (article.name === name));
-  // const client = new MongoClient('mongodb://127.0.0.1');
-  // await client.connect();
-  // const db = client.db('react-blog-db');
   await db.collection('articles').updateOne({ name }, { $inc: { upvote: 1 } });
   const article = await db.collection('articles').findOne({ name });
   if (article) {
-    // article.upvote += 1;
-    res.send(`The ${name} article now got ${article.upvote} upvotes!!!`)
+    res.json(article)
   }
   else {
     res.send('Article not found!');
@@ -40,14 +31,8 @@ app.post('/api/articles/:name/comments', async (req, res) => {
 
   let { name } = req.params;
   let { postedBy, text } = req.body;
-  // let article = articlesInfo.find(a => (a.name === name));
-  // const client = new MongoClient('mongodb://127.0.0.1:27017');
-  // await client.connect();
-  // const db = client.db('react-blog-db');
   const article = await db.collection('articles').findOne({ name });
   if (article) {
-    // let { postBy, text } = req.body;
-    // article.comments.push({ postBy, text });
     await db.collection('articles').updateOne({ name }, { $push: { comments: { postedBy, text } }, });
     res.send(article.comments);
   }
--- myblog-frontend/src/components/ArticleComments.js
+++ myblog-frontend/src/components/ArticleComments.js
@@ -3,7 +3,8 @@ const ArticleComments = ({ comments }) => {
   return (
     <>
       <h3> Comments:</h3>
-      {comments.map(comment => (
+      {console.log(comments)}
+      {comments?.map(comment => (
         <div className="comment" key={comment.postedBy + ':' + comment.text}>
           <h4> {comment.postedBy}</h4>
           <p> {comment.text}</p>
